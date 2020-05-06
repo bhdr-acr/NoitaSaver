0b628a8212c6fcc2045ac4bdadc7fcc4f1dc2b5f
BAHADIR ACAR <bhdr.acr@gmail.com>
2020-05-06 03:13:15 +0300

    Initial commit. 10 Save slots. User can save, load, delete, or browse folders.

---
+++ NoitaSaver/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace NoitaSaver
+{
+    partial class NoitaSaver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NoitaSaver));
+            this.saveSlotGroupBox = new System.Windows.Forms.GroupBox();
+            this.saveSlot10RadioButton = new System.Windows.Forms.RadioButton();
+            this.saveSlot9RadioButton = new System.Windows.Forms.RadioButton();
+            this.saveSlot8RadioButton = new System.Windows.Forms.RadioButton();
+            this.saveSlot7RadioButton = new System.Windows.Forms.RadioButton();
+            this.saveSlot6RadioButton = new System.Windows.Forms.RadioButton();
+            this.saveSlot5RadioButton = new System.Windows.Forms.RadioButton();
+            this.saveSlot4RadioButton = new System.Windows.Forms.RadioButton();
+            this.saveSlot3RadioButton = new System.Windows.Forms.RadioButton();
+            this.saveSlot2RadioButton = new System.Windows.Forms.RadioButton();
+            this.saveSlot1RadioButton = new System.Windows.Forms.RadioButton();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.ShowFolder = new System.Windows.Forms.Button();
+            this.saveSlotGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // saveSlotGroupBox
+            // 
+            this.saveSlotGroupBox.Controls.Add(this.saveSlot10RadioButton);
+            this.saveSlotGroupBox.Controls.Add(this.saveSlot9RadioButton);
+            this.saveSlotGroupBox.Controls.Add(this.saveSlot8RadioButton);
+            this.saveSlotGroupBox.Controls.Add(this.saveSlot7RadioButton);
+            this.saveSlotGroupBox.Controls.Add(this.saveSlot6RadioButton);
+            this.saveSlotGroupBox.Controls.Add(this.saveSlot5RadioButton);
+            this.saveSlotGroupBox.Controls.Add(this.saveSlot4RadioButton);
+            this.saveSlotGroupBox.Controls.Add(this.saveSlot3RadioButton);
+            this.saveSlotGroupBox.Controls.Add(this.saveSlot2RadioButton);
+            this.saveSlotGroupBox.Controls.Add(this.saveSlot1RadioButton);
+            this.saveSlotGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.saveSlotGroupBox.Name = "saveSlotGroupBox";
+            this.saveSlotGroupBox.Size = new System.Drawing.Size(168, 250);
+            this.saveSlotGroupBox.TabIndex = 0;
+            this.saveSlotGroupBox.TabStop = false;
+            this.saveSlotGroupBox.Text = "Save Slots";
+            // 
+            // saveSlot10RadioButton
+            // 
+            this.saveSlot10RadioButton.AutoSize = true;
+            this.saveSlot10RadioButton.Location = new System.Drawing.Point(6, 226);
+            this.saveSlot10RadioButton.Name = "saveSlot10RadioButton";
+            this.saveSlot10RadioButton.Size = new System.Drawing.Size(58, 17);
+            this.saveSlot10RadioButton.TabIndex = 9;
+            this.saveSlot10RadioButton.Text = "Slot 10";
+            this.saveSlot10RadioButton.UseVisualStyleBackColor = true;
+            this.saveSlot10RadioButton.CheckedChanged += new System.EventHandler(this.saveSlotRadioButton_CheckedChanged);
+            // 
+            // saveSlot9RadioButton
+            // 
+            this.saveSlot9RadioButton.AutoSize = true;
+            this.saveSlot9RadioButton.Location = new System.Drawing.Point(6, 203);
+            this.saveSlot9RadioButton.Name = "saveSlot9RadioButton";
+            this.saveSlot9RadioButton.Size = new System.Drawing.Size(52, 17);
+            this.saveSlot9RadioButton.TabIndex = 8;
+            this.saveSlot9RadioButton.Text = "Slot 9";
+            this.saveSlot9RadioButton.UseVisualStyleBackColor = true;
+            this.saveSlot9RadioButton.CheckedChanged += new System.EventHandler(this.saveSlotRadioButton_CheckedChanged);
+            // 
+            // saveSlot8RadioButton
+            // 
+            this.saveSlot8RadioButton.AutoSize = true;
+            this.saveSlot8RadioButton.Location = new System.Drawing.Point(6, 180);
+            this.saveSlot8RadioButton.Name = "saveSlot8RadioButton";
+            this.saveSlot8RadioButton.Size = new System.Drawing.Size(52, 17);
+            this.saveSlot8RadioButton.TabIndex = 7;
+            this.saveSlot8RadioButton.Text = "Slot 8";
+            this.saveSlot8RadioButton.UseVisualStyleBackColor = true;
+            this.saveSlot8RadioButton.CheckedChanged += new System.EventHandler(this.saveSlotRadioButton_CheckedChanged);
+            // 
+            // saveSlot7RadioButton
+            // 
+            this.saveSlot7RadioButton.AutoSize = true;
+            this.saveSlot7RadioButton.Location = new System.Drawing.Point(6, 157);
+            this.saveSlot7RadioButton.Name = "saveSlot7RadioButton";
+            this.saveSlot7RadioButton.Size = new System.Drawing.Size(52, 17);
+            this.saveSlot7RadioButton.TabIndex = 6;
+            this.saveSlot7RadioButton.Text = "Slot 7";
+            this.saveSlot7RadioButton.UseVisualStyleBackColor = true;
+            this.saveSlot7RadioButton.CheckedChanged += new System.EventHandler(this.saveSlotRadioButton_CheckedChanged);
+            // 
+            // saveSlot6RadioButton
+            // 
+            this.saveSlot6RadioButton.AutoSize = true;
+            this.saveSlot6RadioButton.Location = new System.Drawing.Point(6, 134);
+            this.saveSlot6RadioButton.Name = "saveSlot6RadioButton";
+            this.saveSlot6RadioButton.Size = new System.Drawing.Size(52, 17);
+            this.saveSlot6RadioButton.TabIndex = 5;
+            this.saveSlot6RadioButton.Text = "Slot 6";
+            this.saveSlot6RadioButton.UseVisualStyleBackColor = true;
+            this.saveSlot6RadioButton.CheckedChanged += new System.EventHandler(this.saveSlotRadioButton_CheckedChanged);
+            // 
+            // saveSlot5RadioButton
+            // 
+            this.saveSlot5RadioButton.AutoSize = true;
+            this.saveSlot5RadioButton.Location = new System.Drawing.Point(6, 111);
+            this.saveSlot5RadioButton.Name = "saveSlot5RadioButton";
+            this.saveSlot5RadioButton.Size = new System.Drawing.Size(52, 17);
+            this.saveSlot5RadioButton.TabIndex = 4;
+            this.saveSlot5RadioButton.Text = "Slot 5";
+            this.saveSlot5RadioButton.UseVisualStyleBackColor = true;
+            this.saveSlot5RadioButton.CheckedChanged += new System.EventHandler(this.saveSlotRadioButton_CheckedChanged);
+            // 
+            // saveSlot4RadioButton
+            // 
+            this.saveSlot4RadioButton.AutoSize = true;
+            this.saveSlot4RadioButton.Location = new System.Drawing.Point(6, 88);
+            this.saveSlot4RadioButton.Name = "saveSlot4RadioButton";
+            this.saveSlot4RadioButton.Size = new System.Drawing.Size(52, 17);
+            this.saveSlot4RadioButton.TabIndex = 3;
+            this.saveSlot4RadioButton.Text = "Slot 4";
+            this.saveSlot4RadioButton.UseVisualStyleBackColor = true;
+            this.saveSlot4RadioButton.CheckedChanged += new System.EventHandler(this.saveSlotRadioButton_CheckedChanged);
+            // 
+            // saveSlot3RadioButton
+            // 
+            this.saveSlot3RadioButton.AutoSize = true;
+            this.saveSlot3RadioButton.Location = new System.Drawing.Point(6, 65);
+            this.saveSlot3RadioButton.Name = "saveSlot3RadioButton";
+            this.saveSlot3RadioButton.Size = new System.Drawing.Size(52, 17);
+            this.saveSlot3RadioButton.TabIndex = 2;
+            this.saveSlot3RadioButton.Text = "Slot 3";
+            this.saveSlot3RadioButton.UseVisualStyleBackColor = true;
+            this.saveSlot3RadioButton.CheckedChanged += new System.EventHandler(this.saveSlotRadioButton_CheckedChanged);
+            // 
+            // saveSlot2RadioButton
+            // 
+            this.saveSlot2RadioButton.AutoSize = true;
+            this.saveSlot2RadioButton.Location = new System.Drawing.Point(6, 42);
+            this.saveSlot2RadioButton.Name = "saveSlot2RadioButton";
+            this.saveSlot2RadioButton.Size = new System.Drawing.Size(52, 17);
+            this.saveSlot2RadioButton.TabIndex = 1;
+            this.saveSlot2RadioButton.Text = "Slot 2";
+            this.saveSlot2RadioButton.UseVisualStyleBackColor = true;
+            this.saveSlot2RadioButton.CheckedChanged += new System.EventHandler(this.saveSlotRadioButton_CheckedChanged);
+            // 
+            // saveSlot1RadioButton
+            // 
+            this.saveSlot1RadioButton.AutoSize = true;
+            this.saveSlot1RadioButton.Checked = true;
+            this.saveSlot1RadioButton.Location = new System.Drawing.Point(6, 19);
+            this.saveSlot1RadioButton.Name = "saveSlot1RadioButton";
+            this.saveSlot1RadioButton.Size = new System.Drawing.Size(52, 17);
+            this.saveSlot1RadioButton.TabIndex = 0;
+            this.saveSlot1RadioButton.TabStop = true;
+            this.saveSlot1RadioButton.Text = "Slot 1";
+            this.saveSlot1RadioButton.UseVisualStyleBackColor = true;
+            this.saveSlot1RadioButton.CheckedChanged += new System.EventHandler(this.saveSlotRadioButton_CheckedChanged);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(186, 12);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(177, 63);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Enabled = false;
+            this.LoadButton.Location = new System.Drawing.Point(186, 81);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(177, 63);
+            this.LoadButton.TabIndex = 2;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Enabled = false;
+            this.DeleteButton.Location = new System.Drawing.Point(186, 150);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(177, 63);
+            this.DeleteButton.TabIndex = 3;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // ShowFolder
+            // 
+            this.ShowFolder.Location = new System.Drawing.Point(186, 219);
+            this.ShowFolder.Name = "ShowFolder";
+            this.ShowFolder.Size = new System.Drawing.Size(177, 43);
+            this.ShowFolder.TabIndex = 4;
+            this.ShowFolder.Text = "Show Folder";
+            this.ShowFolder.UseVisualStyleBackColor = true;
+            this.ShowFolder.Click += new System.EventHandler(this.ShowFolder_Click);
+            // 
+            // NoitaSaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 270);
+            this.Controls.Add(this.ShowFolder);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.saveSlotGroupBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NoitaSaver";
+            this.Text = "NoitaSaver";
+            this.saveSlotGroupBox.ResumeLayout(false);
+            this.saveSlotGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox saveSlotGroupBox;
+        private System.Windows.Forms.RadioButton saveSlot1RadioButton;
+        private System.Windows.Forms.RadioButton saveSlot10RadioButton;
+        private System.Windows.Forms.RadioButton saveSlot9RadioButton;
+        private System.Windows.Forms.RadioButton saveSlot8RadioButton;
+        private System.Windows.Forms.RadioButton saveSlot7RadioButton;
+        private System.Windows.Forms.RadioButton saveSlot6RadioButton;
+        private System.Windows.Forms.RadioButton saveSlot5RadioButton;
+        private System.Windows.Forms.RadioButton saveSlot4RadioButton;
+        private System.Windows.Forms.RadioButton saveSlot3RadioButton;
+        private System.Windows.Forms.RadioButton saveSlot2RadioButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button ShowFolder;
+    }
+}
+
---
+++ NoitaSaver/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoitaSaver
+{
+    public partial class NoitaSaver : Form
+    {
+        public string UserName;
+
+        public string NollaPath;
+
+        public string SavePath;
+
+        public string SaveSlotSelection = "1";
+
+        public string SecureSavePath;
+
+        public NoitaSaver()
+        {
+            InitializeComponent();
+
+            UserName = Environment.UserName;
+
+            NollaPath = Path.Combine("C:\\Users\\", string.Format("{0}\\AppData\\LocalLow\\Nolla_Games_Noita\\", UserName));
+            SavePath = Path.Combine(NollaPath, "save00\\");
+            SecureSavePath = Path.Combine(NollaPath + SaveSlotSelection);
+            DirectoryInfo directoryInfo = new DirectoryInfo(SecureSavePath);
+
+            if (directoryInfo.Exists)
+            {
+                LoadButton.Enabled = true;
+
+                DeleteButton.Enabled = true;
+            }
+            else
+            {
+                LoadButton.Enabled = false;
+
+                DeleteButton.Enabled = false;
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if (IsGameRunning())
+            {
+                MessageBox.Show("Stop the game first!");
+                return;
+            }
+
+            DialogResult dialogResult = MessageBox.Show("Sure?", "Saving", MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                EmptyFolder(new DirectoryInfo(SecureSavePath));
+
+                CopyFolderSave(SavePath, SecureSavePath);
+
+                LoadButton.Enabled = true;
+
+                DeleteButton.Enabled = true;
+            }
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            if (IsGameRunning())
+            {
+                MessageBox.Show("Stop the game first!");
+                return;
+            }
+
+            DialogResult dialogResult = MessageBox.Show("Sure?", "Loading", MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                EmptyFolder(new DirectoryInfo(SavePath));
+
+                CopyFolderLoad(SecureSavePath, SavePath);
+
+                LoadButton.Enabled = true;
+
+                DeleteButton.Enabled = true;
+            }
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Sure?????", "Deleting", MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                EmptyFolder(new DirectoryInfo(NollaPath + SaveSlotSelection));
+
+                LoadButton.Enabled = true;
+
+                DeleteButton.Enabled = true;
+            }
+        }
+
+        private void EmptyFolder(DirectoryInfo directoryInfo)
+        {
+            if (!directoryInfo.Exists)
+            {
+                Directory.CreateDirectory(directoryInfo.FullName);
+            }
+
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                file.Delete();
+            }
+
+            foreach (DirectoryInfo subfolder in directoryInfo.GetDirectories())
+            {
+                EmptyFolder(subfolder);
+            }
+        }
+
+        public void CopyFolderSave(string SourcePath, string DestinationPath)
+        {
+
+            DirectoryInfo destinationDirectoryInfo = new DirectoryInfo(DestinationPath);
+            if (!destinationDirectoryInfo.Exists)
+            {
+                Directory.CreateDirectory(destinationDirectoryInfo.FullName);
+            }
+
+            //Now Create all of the directories
+            foreach (string dirPath in Directory.GetDirectories(SourcePath, "*",
+                SearchOption.AllDirectories))
+            {
+                string newDirPath = dirPath.Replace("save00", SaveSlotSelection);
+                Directory.CreateDirectory(newDirPath);
+            }
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(SourcePath, "*.*",
+                SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace("save00", SaveSlotSelection), true);
+        }
+
+        public void CopyFolderLoad(string SourcePath, string DestinationPath)
+        {
+
+            DirectoryInfo destinationDirectoryInfo = new DirectoryInfo(DestinationPath);
+            if (!destinationDirectoryInfo.Exists)
+            {
+                Directory.CreateDirectory(destinationDirectoryInfo.FullName);
+            }
+
+            //Now Create all of the directories
+            foreach (string dirPath in Directory.GetDirectories(SourcePath, "*",
+                SearchOption.AllDirectories))
+            {
+                string newDirPath = dirPath.Replace(SaveSlotSelection, "save00");
+                Directory.CreateDirectory(newDirPath);
+            }
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(SourcePath, "*.*",
+                SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(SaveSlotSelection, "save00"), true);
+        }
+
+        private bool IsGameRunning()
+        {
+            bool isRunning = Process.GetProcessesByName("noita")
+                .FirstOrDefault(p => p.MainModule.FileName.StartsWith(@"C:\Program Files (x86)\Steam\steamapps\common\Noita", StringComparison.InvariantCultureIgnoreCase)) != default(Process);
+
+            if (!isRunning)
+                isRunning = Process.GetProcessesByName("noita_dev")
+                    .FirstOrDefault(p => p.MainModule.FileName.StartsWith(@"C:\Program Files (x86)\Steam\steamapps\common\Noita", StringComparison.InvariantCultureIgnoreCase)) != default(Process);
+
+            return isRunning;
+        }
+
+        private void ShowFolder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(SecureSavePath))
+                    Process.Start(SecureSavePath);
+                else
+                    Process.Start(NollaPath);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Seems like you are missing the folder:\n" + NollaPath);
+            }
+        }
+
+        private void saveSlotRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            SaveSlotSelection = radioButton.Text.Substring(5);
+            SecureSavePath = Path.Combine(NollaPath + SaveSlotSelection);
+            DirectoryInfo directoryInfo = new DirectoryInfo(SecureSavePath);
+
+            if (directoryInfo.Exists)
+            {
+                LoadButton.Enabled = true;
+
+                DeleteButton.Enabled = true;
+            }
+            else
+            {
+                LoadButton.Enabled = false;
+
+                DeleteButton.Enabled = false;
+            }
+        }
+    }
+}
